29789bb77ac9c6d57c99cfec7d58fbe472b8a45a
Peter Pham <phamhere@gmail.com>
2018-12-29 13:16:03 -0700

    impleted test for reading users

---
+++ test/reading_test.js
@@ -0,0 +1,16 @@
+const assert = require("assert");
+const User = require("../src/user");
+
+describe("reading users out of database", () => {
+  let joe;
+  beforeEach(done => {
+    joe = new User({ name: "Joe" });
+    joe.save().then(() => done());
+  });
+  it("find all users with name of joe", done => {
+    User.find({ name: "Joe" }).then(users => {
+      assert(users[0]._id.toString() === joe._id.toString());
+      done();
+    });
+  });
+});
