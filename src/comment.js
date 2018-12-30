1a37bf5c30c76a5574b928883c7bd88e7b15e840
Peter Pham <phamhere@gmail.com>
2018-12-29 23:57:43 -0700

    association tests implemented

--- src/comment.js
+++ src/comment.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
   content: String,
-  user: [{ type: Schema.Types.ObjectId, ref: "user" }]
+  user: { type: Schema.Types.ObjectId, ref: "user" }
 });
 
 const Comment = mongoose.model("comment", CommentSchema);
