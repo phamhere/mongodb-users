da4e3db4f1ad9aef68870bde4336708ac92ac07d
Peter Pham <phamhere@gmail.com>
2018-12-29 22:55:25 -0700

    converted postcount to virtual type

--- test/update_test.js
+++ test/update_test.js
@@ -4,7 +4,7 @@ const User = require("../src/user");
 describe("updating records", () => {
   let joe;
   beforeEach(done => {
-    joe = new User({ name: "Joe", postCount: 0 });
+    joe = new User({ name: "Joe", likes: 0 });
     joe.save().then(() => done());
   });
 
@@ -37,10 +37,10 @@ describe("updating records", () => {
   });
 
   it("a user can have their postcount incremented by 1", done => {
-    User.updateMany({ name: "Joe" }, { $inc: { postCount: 1 } })
+    User.updateMany({ name: "Joe" }, { $inc: { likes: 1 } })
       .then(() => User.findOne({ name: "Joe" }))
       .then(user => {
-        assert(user.postCount === 1);
+        assert(user.likes === 1);
         done();
       });
   });
